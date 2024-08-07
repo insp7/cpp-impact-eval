cb3a3b8eed117f800160b56459bddec74667e6e4
insp7 <inspiration601@gmail.com>
2024-08-07 09:53:36 -0400

    refactored code

--- impute.R
+++ impute.R
@@ -8,38 +8,19 @@ root <- find_root(is_rstudio_project)
 
 source("utils.R")
 
-all_hpog_df <- read.csv("AF2--ALL-HPOG.csv")
-pace_only_df <- read.csv("AF2--PACE-ONLY-DataFrame.csv")
-year_up_df <- read.csv("AF2--YEAR-UP-DataFrame.csv")
+all_hpog_imp_set <- read.csv("AF2--ALL-HPOG.csv")
+pace_only_imp_set <- read.csv("AF2--PACE-ONLY-DataFrame.csv")
+year_up_imp_set <- read.csv("AF2--YEAR-UP-DataFrame.csv")
 
-# Discarding variables that I think are unnecessary for the imputation. (Will be confirmed after discussing with JAK)
-all_hpog_imp_set <- all_hpog_df %>% 
-  select(-surveyrespondent_15_hpog, -surveyrespondent_36_hpog, -surveyrespondent_72_hpog,
-         -number_of_services, -behavioral_incentives, -avg_fte_caseload, -childcare_transport, -social_services, -financial_services, 
-         -number_of_employment_supports, -number_of_colocated_services, -emergency_assistance, -peer_support, -number_of_cp_principles, 
-         -proportion_local_some_college, -proportion_local_jobs_health_care, -median_wage_local_health_care, -proportion_local_cash_assistance,
-         -proportion_local_enrolled_school, -total_msa_population, -percent_local_unemployed)
 
-pace_only_imp_set <- pace_only_df %>% 
-  select(-study_year_up, -surveyrespondent_15_pace, -surveyrespondent_36_pace, -surveyrespondent_72_pace)
-
-year_up_imp_set <- year_up_df %>% 
-  select(-study_year_up, -surveyrespondent_15_pace, -surveyrespondent_36_pace, -surveyrespondent_72_pace)
-
-# Imputation for Pace Only WORKS! (TODO: Need to find optimal number of iterations to match closely with the mean. Decide with JAK)
-imp <- mice(pace_only_imp_set, method = 'pmm', m = 1, maxit = 40, seed = 123)
-imputed_pace_only_df <- complete(imp)
-sapply(pace_only_df, calculate_stats)
-sapply(imputed_pace_only_df, calculate_stats)
-
-# Imputation for Year Up WORKS! (TODO: Need to find optimal number of iterations to match closely with the mean. Decide with JAK)
-year_up_imp <- mice(year_up_imp_set, method = 'pmm', m = 1, maxit = 40, seed = 123)
-imputed_year_up_df <- complete(year_up_imp)
-sapply(year_up_imp_set, calculate_stats)
-sapply(imputed_year_up_df, calculate_stats)
-
-# HPOG Imputation: moment of truth
+# HPOG Imputation:
 all_hpog_imp <- mice(all_hpog_imp_set, method = 'pmm', m = 1, maxit = 40, seed = 123)
 imputed_hpog_df <- complete(all_hpog_imp)
-sapply(all_hpog_imp_set, calculate_stats)
-sapply(imputed_hpog_df, calculate_stats)
+
+# PACE Imputation:
+imp <- mice(pace_only_imp_set, method = 'pmm', m = 1, maxit = 40, seed = 123)
+imputed_pace_only_df <- complete(imp)
+
+# YearUp Imputation:
+year_up_imp <- mice(year_up_imp_set, method = 'pmm', m = 1, maxit = 40, seed = 123)
+imputed_year_up_df <- complete(year_up_imp)
--- subset-AF2.R
+++ subset-AF2.R
@@ -7,8 +7,15 @@ all_hpog <- df %>% filter(study_type == 1)
 year_up <- df %>% filter(study_type == 2)
 pace_only <- df %>% filter(study_type == 3)
 
+# Not Including these variables for HPOG: 
+# -surveyrespondent_15_hpog, -surveyrespondent_36_hpog, -surveyrespondent_72_hpog,
+# -number_of_services, -behavioral_incentives, -avg_fte_caseload, -childcare_transport, -social_services, -financial_services, 
+# -number_of_employment_supports, -number_of_colocated_services, -emergency_assistance, -peer_support, -number_of_cp_principles, 
+# -proportion_local_some_college, -proportion_local_jobs_health_care, -median_wage_local_health_care, -proportion_local_cash_assistance,
+# -proportion_local_enrolled_school, -total_msa_population, -percent_local_unemployed
+
 all_hpog_df <- all_hpog %>% 
-  select(participant_id, site_id, surveyrespondent_15_hpog, surveyrespondent_36_hpog, surveyrespondent_72_hpog, 
+  select(participant_id, site_id, 
          credential_15, credential_36, credential_72, 
          currently_working_15,currently_working_36, currently_working_72, 
          hours_worked_per_week_15, hours_worked_per_week_36, hours_worked_per_week_alt_36, hours_worked_per_week_72,
@@ -22,14 +29,14 @@ all_hpog_df <- all_hpog %>%
          marstat_married_0, marstat_widowed_0, marstat_divorced_or_separated_0,
          number_of_children_dependent_0, any_children_0,
          birth_country_usa_0, limited_english_0, worked_before_0_vc, never_worked_0_vc, tanf_assistance_0, wic_or_snap_assistance_0,
-         future_school_part_time, future_school_full_time, future_work, work_hours, career_knowledge_3_variables, life_challenges_index_4_variables,
-         number_of_services, behavioral_incentives, avg_fte_caseload, childcare_transport, social_services, financial_services, 
-         number_of_employment_supports, number_of_colocated_services, emergency_assistance, peer_support, number_of_cp_principles, 
-         proportion_local_some_college, proportion_local_jobs_health_care, median_wage_local_health_care, proportion_local_cash_assistance,
-         proportion_local_enrolled_school, total_msa_population, percent_local_unemployed)
+         future_school_part_time, future_school_full_time, future_work, work_hours, career_knowledge_3_variables, life_challenges_index_4_variables)
+
+
+# Not including these variables for PACE:
+# -study_year_up, -surveyrespondent_15_pace, -surveyrespondent_36_pace, -surveyrespondent_72_pace
 
 pace_only_df <- pace_only %>% 
-  select(participant_id, site_id, surveyrespondent_15_pace, surveyrespondent_36_pace, surveyrespondent_72_pace, study_year_up, 
+  select(participant_id, site_id, 
          currently_working_15, currently_working_36, currently_working_72,
          hours_worked_per_week_15, hours_worked_per_week_36, hours_worked_per_week_alt_36, hours_worked_per_week_72,
          hourly_wage_rate_15, hourly_wage_rate_36, hourly_wage_rate_alt_36, hourly_wage_rate_72,
@@ -52,9 +59,11 @@ pace_only_df <- pace_only %>%
          life_challenges_index_4_variables, life_challenges_index_6_variables, stress_index, weight_15, weight_36, weight_72)
 
 
+# Not including these variables for YearUp:
+# -study_year_up, -surveyrespondent_15_pace, -surveyrespondent_36_pace, -surveyrespondent_72_pace
 
 year_up_df <- year_up %>% 
-  select(participant_id, site_id, surveyrespondent_15_pace, surveyrespondent_36_pace, surveyrespondent_72_pace, study_year_up, 
+  select(participant_id, site_id, 
          currently_working_15, currently_working_36, currently_working_72,
          hours_worked_per_week_15, hours_worked_per_week_36, hours_worked_per_week_alt_36, hours_worked_per_week_72,
          hourly_wage_rate_15, hourly_wage_rate_36, hourly_wage_rate_alt_36, hourly_wage_rate_72,
